86996e1b04155d4c90b01db897a7676075ffe3e1
shivanshuserpentine2001 <63629636+shivanshuserpentine2001@users.noreply.github.com>
2020-04-23 11:27:18 +0530

    Create POINTERSWAPUSINGFUNCT.C

---
+++ POINTERSWAPUSINGFUNCT.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int interchange();
+int main()
+{   int n1 ,*ip;
+    int n2, *dp;
+    int temp;
+
+    printf("enter no 1(n1):");
+    scanf("%d",&n1);
+    printf("enter no 2(n2):");
+    scanf("%d",&n2);
+    ip=&n1;
+    dp=&n2;
+   interchange(ip,dp);}
+int interchange( int *a , int *b)
+ { int temp;
+     temp=a;
+    a=b;
+    b=temp;
+    printf("the given input was swapped in output");
+    printf("\nn1:%d \nn2:%d",*a,*b);
+    printf("\n*******************************\n");
+    }
