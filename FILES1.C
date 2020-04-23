e3250fb6ec2eb0c273a1354fcf3f7096feabfbf2
shivanshuserpentine2001 <63629636+shivanshuserpentine2001@users.noreply.github.com>
2020-04-23 20:44:26 +0530

    Create FILES1.C

---
+++ FILES1.C
@@ -0,0 +1,23 @@
+//FILES PROGRAM OUTPUT IS SHARED USING CAMSCANNER
+#include<stdio.h>
+ int main()
+{   int i=0,data[13];
+    FILE *fp;
+    fp=fopen("INPUT.txt","w");
+    if(fp==0)
+    {
+        printf("error");
+    }
+for(i=0;i<=5;i++)
+{printf("enter data to be copied to file");
+    scanf("%d",&data[i]);
+fprintf(fp,"%d",data[i]);
+}
+printf("the data was inputed into file\n");
+   for(i=0;i<=5;i++)
+    {fscanf(fp,"%d",&data[i]);
+printf("data from file is:%d \n",data[i]);}
+
+ fclose(fp);
+
+ }
